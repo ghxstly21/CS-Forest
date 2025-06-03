fb79e2932ac9a636d7886e4e3a7e385028a13dae
DharmaZR <zalavadiyadharma@gmail.com>
2025-06-03 10:43:06 -0400

    Added all the movement and animation for the player + made the background infinite scroll based on player movement

---
+++ ATCS Forest/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using hi;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementAndCamera : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Transform cameraTransform;
+
+    private Vector2 movement;
+    private PlayerControls controls;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.GamePlay.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+        controls.GamePlay.Move.canceled += ctx => movement = Vector2.zero;
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
+    void Update()
+    {
+        float move = movement.x;
+
+        transform.position += new Vector3(move * moveSpeed * Time.deltaTime, 0f, 0f);
+
+        if (move > 0) spriteRenderer.flipX = false;
+        else if (move < 0) spriteRenderer.flipX = true;
+
+        animator.SetBool("isWalking", move != 0);
+
+        cameraTransform.position = new Vector3(transform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+    }
+}
