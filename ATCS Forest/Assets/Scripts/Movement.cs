39019af4f9405f45f3a6cff76f684b19cba5f4a6
DharmaZR <zalavadiyadharma@gmail.com>
2025-06-04 13:08:50 -0400

    Revert "Extra changes"
    This reverts commit f0d06ef339b80647b6876f0ccd41976b84859da4.

--- ATCS Forest/Assets/Scripts/Movement.cs
+++ ATCS Forest/Assets/Scripts/Movement.cs
@@ -1,39 +1,33 @@
+using hi;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using hi;
 
-public class PlayerMovementAndCamera : MonoBehaviour
+public class PlayerMovement2D : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Animator animator;
-    public SpriteRenderer spriteRenderer;
-    public Transform cameraTransform;
+    public float speed = 5f;
 
-    private Vector2 movement;
-    private PlayerControls controls;
+    Rigidbody2D rb;
+    PlayerControls controls;
 
     void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         controls = new PlayerControls();
-
-        controls.GamePlay.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
-        controls.GamePlay.Move.canceled += ctx => movement = Vector2.zero;
     }
 
-    void OnEnable() => controls.Enable();
-    void OnDisable() => controls.Disable();
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
 
     void Update()
     {
-        float move = movement.x;
-
-        transform.position += new Vector3(move * moveSpeed * Time.deltaTime, 0f, 0f);
-
-        if (move > 0) spriteRenderer.flipX = false;
-        else if (move < 0) spriteRenderer.flipX = true;
-
-        animator.SetBool("isWalking", move != 0);
-
-        cameraTransform.position = new Vector3(transform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+        Vector2 input = controls.GamePlay.Move.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector2(input.x * speed, rb.linearVelocity.y);
     }
 }
