f0d06ef339b80647b6876f0ccd41976b84859da4
DharmaZR <zalavadiyadharma@gmail.com>
2025-06-04 12:43:06 -0400

    Extra changes

--- ATCS Forest/Assets/Scripts/Movement.cs
+++ ATCS Forest/Assets/Scripts/Movement.cs
@@ -1,33 +1,39 @@
-using hi;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using hi;
 
-public class PlayerMovement2D : MonoBehaviour
+public class PlayerMovementAndCamera : MonoBehaviour
 {
-    public float speed = 5f;
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Transform cameraTransform;
 
-    Rigidbody2D rb;
-    PlayerControls controls;
+    private Vector2 movement;
+    private PlayerControls controls;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         controls = new PlayerControls();
+
+        controls.GamePlay.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+        controls.GamePlay.Move.canceled += ctx => movement = Vector2.zero;
     }
 
-    void OnEnable()
-    {
-        controls.Enable();
-    }
-
-    void OnDisable()
-    {
-        controls.Disable();
-    }
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
 
     void Update()
     {
-        Vector2 input = controls.GamePlay.Move.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector2(input.x * speed, rb.linearVelocity.y);
+        float move = movement.x;
+
+        transform.position += new Vector3(move * moveSpeed * Time.deltaTime, 0f, 0f);
+
+        if (move > 0) spriteRenderer.flipX = false;
+        else if (move < 0) spriteRenderer.flipX = true;
+
+        animator.SetBool("isWalking", move != 0);
+
+        cameraTransform.position = new Vector3(transform.position.x, cameraTransform.position.y, cameraTransform.position.z);
     }
 }
